f8a5aa5175ea3edcd7fac645734bfd813d0530db
Varun Patil <radialapps@gmail.com>
2020-02-11 04:49:21 +0000

    Count some vote

--- views/vote.php
+++ views/vote.php
@@ -2,10 +2,30 @@
 require_once "bootstrap.php";
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    print_r($_POST);
-    exit();
+    $election = $entityManager->find('Election', $_POST['id']);
+    if ($election === null) { echo "No such election"; die(); }
+
+    foreach ($election->getPosts() as $post) {
+        if ($post->isYNN()) {
+            foreach ($post->getCandidates() as $candidate) {
+                $vote = $_POST['c-' . $candidate->getId()];
+                if (!isset($vote)) {
+                    echo "Didn't vote for " . $candidate->getName(); die();
+                }
+
+                echo "Voted for candidate as #$vote";
+            }
+        } else {
+            $vote = $_POST['p-' . $post->getId()];
+            if (!isset($vote)) {
+                echo "Didn't vote for " . $post->getName(); die();
+            }
+            echo "Voted for candidate #$vote";
+        }
+    }
+    die();
 }
 
-$elections = $entityManager->getRepository('Election')->findAll();
+$election = $entityManager->getRepository('Election')->findAll()[0];
 
-echo $twig->render('vote.html', ['elections' => $elections]);
+echo $twig->render('vote.html', ['election' => $election]);
