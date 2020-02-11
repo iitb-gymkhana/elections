3f22b6bf20e50e09260a158520b7d7851bc4e3b4
Varun Patil <radialapps@gmail.com>
2020-02-11 04:35:01 +0000

    Get votes

--- views/vote.php
+++ views/vote.php
@@ -1,6 +1,11 @@
 <?php
 require_once "bootstrap.php";
 
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    print_r($_POST);
+    exit();
+}
+
 $elections = $entityManager->getRepository('Election')->findAll();
 
 echo $twig->render('vote.html', ['elections' => $elections]);
