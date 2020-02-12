2d55f5711763ce8c491a1b3267fc23e452768646
Varun Patil <radialapps@gmail.com>
2020-02-12 00:36:03 +0000

    Show eveything in preview

--- views/preview.php
+++ views/preview.php
@@ -7,4 +7,7 @@ $election = $entityManager->find('Election', $electionId);
 if ($election === null) { echo "No such election"; die(); }
 if (!$USER_SUPERADMIN && $election->getCreator() !== $USER_ROLL) dieNoElection();
 
-echo $twig->render('vote.html', ['election' => $election ]);
+echo $twig->render('vote.html', [
+    'election' => $election,
+    'preview' => true,
+]);
